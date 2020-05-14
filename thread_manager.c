3e3e7a1829995aac1108f1e3f340ec79646aea6f
Brian L <brianla7755@gmail.com>
2020-05-14 16:54:54 -0700

    Update thread_manager.c

--- thread_manager.c
+++ thread_manager.c
@@ -4,140 +4,165 @@
 #include <pthread.h>
 #include <stdbool.h>
 #include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-// TODO: read in stdin and store into the head of linked list.
-//thread mutex lock for access to the log index.
-// sample output: "Logindex 1, thread 2, PID 5435, DATE TIME: Head of the linked list contains line foo."
+//thread mutex lock for access to the log index
 pthread_mutex_t tlock1 = PTHREAD_MUTEX_INITIALIZER;
 //thread mutex lock for critical sections of allocating THREADDATA
-pthread_mutex_t tlock2 = PTHREAD_MUTEX_INITIALIZER; 
+pthread_mutex_t tlock2 = PTHREAD_MUTEX_INITIALIZER;
+//thread mutex lock for critical sections of allocating THREADDATA
+pthread_mutex_t tlock3 = PTHREAD_MUTEX_INITIALIZER;
 
 void* thread_runner(void*);
-void print_current_time(void);
 pthread_t tid1, tid2;
-struct THREADDATA_STRUCT
-{
-  pthread_t creator;
+struct THREADDATA_STRUCT{
+    pthread_t creator;
+
 };
 typedef struct THREADDATA_STRUCT THREADDATA;
 
-THREADDATA* p=NULL;
+THREADDATA* p=NULL;     //default
 
+
+//A flag to indicate if the reading of input is complete,
+//so the other thread knows when to stop
+bool is_reading_complete = false;
 
 //variable for indexing of messages by the logging function
 int logindex=0;
 int *logip = &logindex;
 
+//for fgets...
+char buffer[100];
+char *result;
 
-//A flag to indicate if the reading of input is complete, 
-//so the other thread knows when to stop
-bool is_reading_complete = false;
 
-
-/*********************************************************
-// function main  ------------------------------------------------- 
-*********************************************************/
-int main() {
+//functions
+void logger(int* i, pthread_t m, THREADDATA* p);
+/**********************************************************
+ * function main  ------------------------------------------------- *********************************************************/
+int main(){
     printf("create first thread\n");
     pthread_create(&tid1,NULL,thread_runner,NULL);
-  
     printf("create second thread\n");
     pthread_create(&tid2,NULL,thread_runner,NULL);
-  
     printf("wait for first thread to exit\n");
+
+    //wait
     pthread_join(tid1,NULL);
+
     printf("first thread exited\n");
-
     printf("wait for second thread to exit\n");
-    pthread_join(tid2,NULL);
-    printf("second thread exited\n");
 
+    pthread_join(tid2,NULL);
+
+    printf("second thread exited\n");
     exit(0);
 
 }//end main
 
 
 /**********************************************************************
-// function thread_runner runs inside each thread -------------------------------------------------- 
-**********************************************************************/
-void* thread_runner(void* x)
-{
+function thread_runner runs inside each thread -------------------------------------------------- **********************************************************************/
+
+void* thread_runner(void* x){
     pthread_t me;
-    char input[100];
-    char *result;
     me = pthread_self();
-    printf("This is thread %ld (p=%p)\n",me,p);
-  
-    pthread_mutex_lock(&tlock2); // critical section starts
+
+    logger(logip, me, p);               //log
+    printf("enter thread stream\n");
+
+
+    pthread_mutex_lock(&tlock2);
+    // critical section starts - malloc occurs if p = NULL
     if (p==NULL) {
-	p = (THREADDATA*) malloc(sizeof(THREADDATA));
-    	p->creator=me;
+        p = (THREADDATA*)malloc(sizeof(THREADDATA));
+        p->creator=me;
+        logger(logip, me, p);               //log
+        printf("created THREADDATA\n");
     }
     pthread_mutex_unlock(&tlock2);  // critical section ends
-    
-    // TODO CommandNode *command
-    if (p!=NULL && p->creator==me) {// this is thread 1 right here
-	// this code block here should read user input and store it in variable result. 
-	while ((result = fgets(input, 100, stdin)) != NULL) 
-   	    if (*result == '\n') break;
-    	printf("This is thread %ld and I created the THREADDATA %p\n",me,p);
-    } else {
-	// before printing any log messages, mutex_lock(locklogindex) so it does not mess up the counting.
-	// before updating the head from thread 1 use mutex_lock(lockhead) and then mutex_unlock(lockhead).
-	// something like while (!is_read_complete) { sleep(2);
-		// check if head is different from last time, if yes then print the head.
-	// print out if different from head?
-	print_current_time();
-	printf("This is thread %ld and I can access the THREADDATA %p\n",me,p);
+
+
+    while ((result = fgets(buffer, 100, stdin)) != NULL) {
+        if (*result == '\n')
+            break;    //break immediately
+        //first thread - check for string
+        if (p!=NULL && p->creator==me) {
+
+            //critical section of incrementation
+            pthread_mutex_lock(&tlock1);
+            logindex++;
+            pthread_mutex_unlock(&tlock1);      //end crit section
+
+            logger(logip, me, p);           //log
+            printf("OK %s\n", buffer);
+        }   //end if
+            //TODO add to linked list node here, might need mutex?
+            //is_reading_complete = true;     //set true
+        else    {       //second thread: thread spinning
+            //while(is_reading_complete == false); {
+            sleep(0.2);    //wait 2s: reduce ttl CPU cycles
+            //TODO print node
+            printf("OK #2 %s\n", buffer);
+            //}
+        }
     }
 
 
-    // TODO use mutex to make this a start of a critical section 
-    pthread_mutex_lock(&tlock1);
+    // TODO use mutex to make this a start of a critical section
+    //pthread_mutex_lock(&tlock3);
     if (p!=NULL && p->creator==me)
-    	printf("This is thread %ld and I didnot touch THREADDATA\n",me);
-    else {
-    /**
-     * TODO Free the THREADATA object. Freeing should be done by the other thread from the one that created it.
-     * See how the THREADDATA was created for an example of how this is done.
-     */ pthread_mutex_lock(&lock);
-    	free(p);
-    	printf("This is thread %ld and I deleted the THREADDATA\n",me);
-	pthread_mutex_unlock(&lock);
+        printf("This is thread %ld and I did not touch THREADDATA\n", me);
+    else{
+        /**   * TODO Free the THREADATA object. Freeing should be done by the other thread from the one that created it.
+         * See how the THREADDATA was created for an example of how this is done.   */
+        printf("This is thread %ld and I deleted the THREADDATA\n", me);
     }
-    // TODO critical section ends
-    pthread_mutex_unlock(&tlock1);
-    print_current_time();
+    //pthread_mutex_unlock(&tlock3);
+
     pthread_exit(NULL);
     return NULL;
 
-} //end thread_runner
+}//end thread_runner
 
-void print_current_time(void) {
-    // variables to store data and time components
+
+void logger(int* i, pthread_t m, THREADDATA* p) {
+    // variables to store date and time components
     int hours, minutes, seconds, day, month, year;
-    // time_t is an arithmetic time type
+
+    // time_t is arithmetic time type
     time_t now;
-    // obtain the current time
+    // localtime converts a time_t value to calendar time and
+    // returns a pointer to a tm structure with its members
+    // filled with the corresponding values
+    struct tm *local;
+
+    // Obtain current time
     // time() returns the current time of the system as a time_t value
     time(&now);
-    // localtime converts a time_t value to calender time and 
-    // returns a pointer to a tm structure with its members
-    // filled with a corresponding values
-    struct tm *local = localtime(&now);
+    //acquire current time
+    local = localtime(&now);
 
-    hours = local->tm_hour;
-    minutes = local->tm_min;
-    seconds = local->tm_sec;
 
-    day = local->tm_mday;
-    month = local->tm_mon + 1;
-    year = local->tm_year + 1900;
+    //get time
+    hours = local->tm_hour;        // get hours since midnight (0-23)
+    minutes = local->tm_min;       // get minutes passed after the hour (0-59)
+    seconds = local->tm_sec;       // get seconds passed after minute (0-59)
+
+    //get date
+    day = local->tm_mday;          // get day of month (1 to 31)
+    month = local->tm_mon + 1;     // get month of year (0 to 11)
+    year = local->tm_year + 1900;  // get year since 1900
+
+    //critical section of incrementation
+    pthread_mutex_lock(&tlock1);
+    printf("Logindex %d, thread %ld (p=%p), PID %d, %02d/%02d/%d %02d:%02d:%02d: ", ++*i, m, p, getpid(), month, day, year, hours, minutes, seconds);
+    pthread_mutex_unlock(&tlock1);      //end crit section
+
+    //printf("%02d/%02d/%d %02d:%02d:%02d: ", month, day, year, hours, minutes, seconds);
+
+}
+
     
-    printf("Date: %02d/%02d/%d ", month, day, year);
-    if (hours < 12)
-	printf("Time: %02d:%02d:%02d am\n", hours, minutes, seconds);
-    else
-	printf("Time: %02d:%02d:%02d pm\n", hours - 12, minutes, seconds);
-
-} 
